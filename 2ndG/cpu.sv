5b82bbdf02bc432559aafcad95df48bec8767230
Tokio Kajitsuka <kajitsuka-tokio@g.ecc.u-tokyo.ac.jp>
2019-02-19 11:40:21 +0900

    微調整

--- 2ndG/cpu.sv
+++ 2ndG/cpu.sv
@@ -16,9 +16,9 @@ module cpu (
     wire [31:0] fetch_result    [0:SUBCORE_NUM-1];
 
     main m(subcore_ended, fetch_result, u_n_in, l_n_in, exec_requested, requested_pc, fetch_addr, rx, tx, clk, rstn);
-    sub #(.CORE_NUM(0), .DATA_FILE("/home/tongari/cpu/core/2ndG/child0.txt")) s (exec_requested[0], requested_pc[0], fetch_addr[0], u_n_in[0], l_n_in[0], subcore_ended[0], fetch_result[0], clk, rstn);
-    sub #(.CORE_NUM(1), .DATA_FILE("/home/tongari/cpu/core/2ndG/child1.txt")) s (exec_requested[1], requested_pc[1], fetch_addr[1], u_n_in[1], l_n_in[1], subcore_ended[1], fetch_result[1], clk, rstn);
-    sub #(.CORE_NUM(2), .DATA_FILE("/home/tongari/cpu/core/2ndG/child2.txt")) s (exec_requested[2], requested_pc[2], fetch_addr[2], u_n_in[2], l_n_in[2], subcore_ended[2], fetch_result[2], clk, rstn);
-    sub #(.CORE_NUM(3), .DATA_FILE("/home/tongari/cpu/core/2ndG/child3.txt")) s (exec_requested[3], requested_pc[3], fetch_addr[3], u_n_in[3], l_n_in[3], subcore_ended[3], fetch_result[3], clk, rstn);
+    sub #(.CORE_NUM(0), .DATA_FILE("/home/tongari/cpu/core/2ndG/child0.txt")) s0 (exec_requested[0], requested_pc[0], fetch_addr[0], u_n_in[0], l_n_in[0], subcore_ended[0], fetch_result[0], clk, rstn);
+    sub #(.CORE_NUM(1), .DATA_FILE("/home/tongari/cpu/core/2ndG/child1.txt")) s1 (exec_requested[1], requested_pc[1], fetch_addr[1], u_n_in[1], l_n_in[1], subcore_ended[1], fetch_result[1], clk, rstn);
+    sub #(.CORE_NUM(2), .DATA_FILE("/home/tongari/cpu/core/2ndG/child2.txt")) s2 (exec_requested[2], requested_pc[2], fetch_addr[2], u_n_in[2], l_n_in[2], subcore_ended[2], fetch_result[2], clk, rstn);
+    sub #(.CORE_NUM(3), .DATA_FILE("/home/tongari/cpu/core/2ndG/child3.txt")) s3 (exec_requested[3], requested_pc[3], fetch_addr[3], u_n_in[3], l_n_in[3], subcore_ended[3], fetch_result[3], clk, rstn);
     
 endmodule
